9c6ccfe9abdac1d90c6a4aea4c44a5e352d07d95
xmiolo <ggrbedin@gmail.com>
2023-05-29 20:32:27 -0300

    Estrutura inicial

---
+++ src/main/java/com/auto/crudator/CrudatorApplication.java
@@ -0,0 +1,13 @@
+package com.auto.crudator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auto/crudator/CrudatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.crudator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
